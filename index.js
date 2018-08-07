0580857a3f7f56979d7fb577a9afc027ad194eeb
Franco <Franco.Fernando@bcg.com>
2018-08-07 09:34:29 -0400

    trying to print user agent

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hola BCI, somo BCG y Platinion! \n" 
-    	+ "Tu navegador es ---->" + request.headers['user-agent']);
+    response.end("Hello Mr. Consultant! \n" 
+    	+ "You are visiting us from ---->" + request.headers['user-agent']);
 
 });
 
