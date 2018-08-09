91b3ceb0a111e3472bef61a82594cf89a0eb602e
Franco <Franco.Fernando@bcg.com>
2018-08-09 12:39:24 -0400

    bleh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Mr. Consultant! \n\n" 
+    response.end("Nacho puto! \n\n" 
     	+ "You are visiting us using ----> " + request.headers['user-agent']);
 
 });
