27b8a2c61a5cd53f17c2260c7fb7b443924b293e
fer.gabriel.franco <fer.gabriel.franco@gmail.com>
2018-08-22 13:08:52 -0300

    Added embeded iframe with chatbot

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ var server = http.createServer(function(req, res) {
 	//res.writeHead(200, {"Content-Type": "application/json"});
     //res.end(JSON.stringify({ click-me : "https://i.kym-cdn.com/entries/icons/original/000/021/807/4d7.png" }, null, 3));
 
-	var imgUrl = "<html><body><img title='elliot' src='https://i.kym-cdn.com/entries/icons/original/000/021/807/4d7.png'/></body></html>"
+	//var imgUrl = "<html><body><img title='elliot' src='https://i.kym-cdn.com/entries/icons/original/000/021/807/4d7.png'/></body></html>"
+
+    var html = "<html lang='en' xmlns='http://www.w3.org/1999/xhtml'><head><meta charset='utf-8' /><title></title></head><body><iframe src='https://webchat.botframework.com/embed/ferchola?s=GEn4FGHlR-s.cwA.6XI.foiiMJbTBoFW7Dahva0bWX2gPqw3KOGwbUTsCNzWmwA' width='400' height='550'></iframe></body></html>";
 
    	res.writeHead(200, {"Content-Type": "text/html"});
-    res.end(imgUrl)
+    res.end(html);
 
 });
 
